be0976dc6beffe2a36cef4be2849e34fb408631b
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-07 18:14:11 +0200

    Make taxi phone app permanent and non-removable

--- lb-phone-config/zz_taxi_ruf.lua
+++ lb-phone-config/zz_taxi_ruf.lua
@@ -1,16 +1,20 @@
--- Taxi-Ruf fuer lb-phone 2.3.7
+-- Taxi-Ruf fuer lb-phone
 -- Diese Datei nach lb-phone/config/zz_taxi_ruf.lua kopieren.
--- Wichtig: keine ui/iframe-Einbindung. Die App startet nur die funktionierende lst_phone_taxi-NUI.
+-- Permanente Custom-App, nicht loeschbar.
+-- Keine ui/iframe-Einbindung: Beim Antippen startet die stabile lst_phone_taxi-NUI.
 
 Config.CustomApps = Config.CustomApps or {}
 
 Config.CustomApps["TaxiRuf"] = {
     name = "Taxi-Ruf",
     description = "Taxi rufen",
+    developer = "Los Santos Taxi",
     icon = "https://cdn-icons-png.flaticon.com/512/3097/3097180.png",
+    images = {},
     size = 512,
-    removable = false,
     defaultApp = true,
+    removable = false,
+    keepOpen = false,
 
     onUse = function()
         ExecuteCommand("taxiapp")
