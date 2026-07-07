9621a1d60f2be6c4720fdd241a2979ddd8fb4c35
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-07 18:32:38 +0200

    Add phone app UI file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'lst_phone_taxi'
 description 'Los Santos Taxi App fuer lb-phone'
 author 'Los Santos Taxi'
-version '1.0.0'
+version '1.1.0'
 
 lua54 'yes'
 
@@ -12,6 +12,7 @@ ui_page 'html/index.html'
 
 files {
     'html/index.html',
+    'html/phone.html',
     'html/style.css',
     'html/app.js'
 }
