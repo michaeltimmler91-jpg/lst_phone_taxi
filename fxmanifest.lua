4169ba4d6514593af96656568b5ebdbfe0a4e39e
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-07 19:57:06 +0200

    Include phone app icon file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 name 'lst_phone_taxi'
 description 'Los Santos Taxi App fuer lb-phone'
 author 'Los Santos Taxi'
-version '1.1.0'
+version '1.1.1'
 
 lua54 'yes'
 
@@ -14,7 +14,9 @@ files {
     'html/index.html',
     'html/phone.html',
     'html/style.css',
-    'html/app.js'
+    'html/phone-polish.css',
+    'html/app.js',
+    'html/icon.png'
 }
 
 shared_scripts {
