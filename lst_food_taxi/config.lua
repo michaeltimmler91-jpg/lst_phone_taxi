6627cbfe356342bd2be2f9e26ae9c906769f25ca
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-10 17:10:00 +0200

    Add food app configuration

---
+++ lst_food_taxi/config.lua
@@ -0,0 +1,30 @@
+Config = {}
+
+Config.Debug = true
+Config.PhoneRegisterDelayMs = 8000
+
+Config.SupabaseUrl = 'https://unkfqoplynwabulnzpar.supabase.co'
+Config.SupabaseAnonKey = 'sb_publishable_AkIVrLBsgIV2jYJ5gGsBmw_f7P62KTK'
+
+Config.AllowedJobs = {
+    burgershot = {
+        label = 'Burger Shot',
+        pickup = 'Burger Shot'
+    },
+    pizzeria = {
+        label = 'Pizzeria',
+        pickup = 'Pizzeria'
+    },
+    upnatom = {
+        label = 'Up-n-Atom',
+        pickup = 'Up-n-Atom'
+    },
+    beanmachine = {
+        label = 'Bean Machine',
+        pickup = 'Bean Machine'
+    },
+    uwucafe = {
+        label = 'UwU Café',
+        pickup = 'UwU Café'
+    }
+}
